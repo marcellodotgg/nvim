bd366e31e64dc7262ef402a43e350e898e628d05
Marcello <hey@marcello.gg>
2024-11-27 19:05:02 -0500

    change the color pack to catppuccin

--- after/plugin/colors.lua
+++ after/plugin/colors.lua
@@ -1,8 +1,5 @@
 function ColorMyPencils(color)
-    color = color or "tokyonight"
-    vim.g.tokyonight_style = "night"
-    vim.g.tokyonight_transparent = false
-    vim.g.tokyonight_italic_functions = true
+    color = color or "catppuccin"
     vim.cmd.colorscheme(color)
 end
 
