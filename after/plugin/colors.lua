2ce8c134df35691ce70feda09bbf97c0e82a107a
Marcello <hey@marcello.gg>
2025-01-07 21:41:57 -0500

    use github dark theme

--- after/plugin/colors.lua
+++ after/plugin/colors.lua
@@ -1,5 +1,5 @@
 function ColorMyPencils(color)
-    color = color or "catppuccin"
+    color = color or "github_dark"
     vim.cmd.colorscheme(color)
 end
 
--- lua/marcello/packer.lua
+++ lua/marcello/packer.lua
@@ -7,6 +7,7 @@ return require('packer').startup(function(use)
     -- Packer can manage itself
     use 'wbthomason/packer.nvim'
     use 'folke/tokyonight.nvim'
+    use 'projekt0n/github-nvim-theme'
 
     use {
         'jose-elias-alvarez/null-ls.nvim',
